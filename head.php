84f1821766501b7fc9803af8e0ae86e03331482c
borja andrés <borya09@hotmail.com>
2016-04-02 22:53:05 +0200

    add viewport meta

--- head.php
+++ head.php
@@ -1,4 +1,5 @@
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href='https://fonts.googleapis.com/css?family=Open+Sans:200,400,600,800' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" href="styles/main.css"/>
 <script src="javascripts/vendor/jquery/jquery.min.js"></script>
