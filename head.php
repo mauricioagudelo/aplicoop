72078a4a55ea0c2410e028b1ebcfa56faa115cc2
Leonidas Ioannidis <leonidas@cryptolab.net>
2018-06-24 14:23:59 +0200

    move image files

--- head.php
+++ head.php
@@ -1,8 +1,8 @@
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="apple-touch-icon" sizes="76x76" href="imatges/apple-touch-icon.png">
-<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+<link rel="icon" type="image/png" href="imatges/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="imatges/favicon-16x16.png" sizes="16x16">
 <link href='https://fonts.googleapis.com/css?family=Open+Sans:200,400,600,800' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" href="styles/main.css"/>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
